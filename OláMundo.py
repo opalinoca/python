ed825b093002591059f008275390ca495bbaec08
Aline <aline.vieira77@yahoo.com>
2021-07-14 18:18:52 -0300

    python

---
+++ OláMundo.py
@@ -0,0 +1,5 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello, World!')
+Hello, World!
+>>> 
---
+++ teste1.py
@@ -0,0 +1,4 @@
+nome= input('Qual seu nome?')
+peso= input('Qual seu peso?')
+idade= input('Qual sua idade?')
+print(nome, idade, peso)
